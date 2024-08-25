34e7f61915a11f6872934d74fa40e9bbb4b00372
halmakey <916773+halmakey@users.noreply.github.com>
2024-08-26 04:43:10 +0900

    actions

---
+++ actions/collection.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import { getRequestContext } from "@cloudflare/next-on-pages";
+import { nanoid } from "nanoid";
+import { redirect } from "next/navigation";
+import {
+  clearCollectionImages,
+} from "./image";
+
+export async function createCollection() {
+  const db = getRequestContext().env.DB;
+
+  const id = nanoid();
+  const now = Date.now();
+  await db
+    .prepare(
+      "INSERT INTO collection (id, created_at, updated_at) VALUES (?, ?, ?)",
+    )
+    .bind(id, now, now)
+    .run();
+
+  redirect(`/collection/${id}`);
+}
+
+export async function deleteCollection(collectionId: string) {
+  const db = getRequestContext().env.DB;
+
+  await Promise.all([
+    clearCollectionImages(collectionId),
+    db.prepare("DELETE FROM collection WHERE id = ?").bind(collectionId).run(),
+  ]);
+}
+
+export async function getCollection(collectionId: string) {
+  const db = getRequestContext().env.DB;
+  const result = await db
+    .prepare("SELECT id, updated_at FROM collection WHERE id = ?")
+    .bind(collectionId)
+    .first<{ id: string; updated_at: number }>();
+  return result;
+}
+
+export async function listAllCollections() {
+  const db = getRequestContext().env.DB;
+  const result = await db
+    .prepare("SELECT id, updated_at FROM collection ORDER BY updated_at DESC")
+    .all<{ id: string; updated_at: number }>();
+  return result.results;
+}
---
+++ actions/image.ts
@@ -0,0 +1,103 @@
+"use server";
+
+import { getRequestContext } from "@cloudflare/next-on-pages";
+import { nanoid } from "nanoid";
+
+export async function addImage(
+  {
+    collectionId,
+    index,
+    width,
+    height,
+  }: {
+    collectionId: string;
+    index: number;
+    width: number;
+    height: number;
+  },
+  formData: FormData,
+) {
+  const db = getRequestContext().env.DB;
+  const data = getRequestContext().env.DATA;
+
+  const blob = formData.get("data") as Blob;
+
+  const id = nanoid();
+  const now = Date.now();
+
+  await Promise.all([
+    db
+      .prepare(
+        "INSERT INTO collection_image (id, collection_id, image_index, width, height, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?)",
+      )
+      .bind(id, collectionId, index, width, height, now, now)
+      .run(),
+    data.put("/image/" + id, blob, {
+      httpMetadata: { contentType: blob.type },
+    }),
+  ]);
+
+  return { id };
+}
+
+export async function deleteImage(collectionId: string, imageId: string) {
+  const db = getRequestContext().env.DB;
+  const data = getRequestContext().env.DATA;
+
+  const otherImages = await listImagesForCollection(collectionId);
+  let index = otherImages.findIndex((image) => image.id === imageId);
+  if (index === -1) {
+    index = Infinity;
+  }
+  const updateIndexImages = otherImages.slice(index + 1);
+
+  await Promise.all([
+    db.batch([
+      db.prepare("DELETE FROM collection_image WHERE id = ?").bind(imageId),
+      ...updateIndexImages.map((image) =>
+        db
+          .prepare("UPDATE collection_image SET image_index = ? WHERE id = ?")
+          .bind(image.image_index - 1, image.id),
+      ),
+    ]),
+    data.delete("/image/" + imageId).catch((err) => {
+      console.warn(err);
+    }),
+  ]);
+}
+
+export async function clearCollectionImages(collectionId: string) {
+  const db = getRequestContext().env.DB;
+  const data = getRequestContext().env.DATA;
+
+  const images = await listImagesForCollection(collectionId);
+
+  await Promise.all(
+    images.map(({ id }) =>
+      Promise.all([
+        db.prepare("DELETE FROM collection_image WHERE id = ?").bind(id).run(),
+        data.delete("/image/" + id).catch((err) => {
+          console.warn(err);
+        }),
+      ]),
+    ),
+  );
+}
+
+export async function listImagesForCollection(collectionId: string) {
+  const db = getRequestContext().env.DB;
+  const result = await db
+    .prepare(
+      "SELECT id, collection_id, image_index, width, height, updated_at FROM collection_image WHERE collection_id = ? ORDER BY image_index, updated_at",
+    )
+    .bind(collectionId)
+    .all<{
+      id: string;
+      collection_id: string;
+      image_index: number;
+      width: number;
+      height: number;
+      updated_at: number;
+    }>();
+  return result.results;
+}
